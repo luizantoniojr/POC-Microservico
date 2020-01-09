fb229b9afa8cb631a1ea9022ce863c56776f362c
luizantoniojr <luizantoniosilvajr@outlook.com>
2020-01-08 23:01:08 -0300

    Criação da api

---
+++ src/ClienteAPI/Cliente.API/Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cliente.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+      
+        public ClienteController()
+        {
+        }
+
+        [HttpPost]
+        public bool Post()
+        {
+            return true;
+        }
+    }
+}
